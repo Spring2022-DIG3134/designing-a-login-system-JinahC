66d31ed19e7fc1f883b4862677a500d60f5b3df6
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-08 13:09:49 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+    include("security.php")
+?>
+
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action='login.php' method='POST'>
+        <label for='username'>USERNAME<label><br>
+        <input type='text' name='username' id='username'><br><br>
+        <label for='password'>PASSWORD<label><br>
+        <input type='password' name='password' id='password'><br><br>
+        <input type='submit' name='submit' value='LOGIN'>
+    </form>
+    <?php
+        if (security_validate()) {
+            security_login();
+
+            if (security_loggedIn()) {
+                echo("<br><br>Success");
+                echo("<br><a href='logout.php'>Logout</a>");
+            }
+            else 
+                echo("<br><br>Username or password is wrong.");
+        }
+    ?>
+    
+</body>
+</html>
