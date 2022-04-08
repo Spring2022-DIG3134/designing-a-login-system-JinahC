b2ed99f2df9f62e3b5e50418f219466fca25e370
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-08 13:10:03 -0400

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,23 @@
+<?php
+    include("security.php")
+?>
+
+<html>
+<head>
+    <title>Sign Up</title>
+</head>
+<body>
+    <h2>Signup</h2>
+    <form action="signup.php" method="POST">
+        <label for="username">USERNAME<label><br>
+        <input type="text" name="username" id="username"><br><br>
+        <label for="password">PASSWORD<label><br>
+        <input type="password" name="password" id="password"><br><br>
+        <input type="submit" name="submit" value="Sign Up">
+    </form>
+    <?php
+        if (security_validate())
+            security_addNewUser();
+    ?>
+</body>
+</html>
